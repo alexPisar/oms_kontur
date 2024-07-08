786d34b595e72067372a1f764face52b44a157f0
alexPisar <support.db@wera-rus.com>
2024-07-08 11:55:51 +1000

    Сделал ежедневный переброс кодов ЭДО, которых нет в EDI, но есть в Трейдере

--- EdiProcessingUnit/ProcessorUnits/ExecuteEdiProceduresProcessor.cs
+++ EdiProcessingUnit/ProcessorUnits/ExecuteEdiProceduresProcessor.cs
@@ -15,6 +15,24 @@ namespace EdiProcessingUnit.ProcessorUnits
         public override void Run()
         {
             _ediDbContext.ExecuteProcedure("EDI.TRANSFER_GOOD_MAPPING_FROM_EDI");
+
+            var currentDateTime = DateTime.Now;
+            DateTime dateTimeFrom = new DateTime(
+                currentDateTime.Year, currentDateTime.Month, currentDateTime.Day, 18, 0, 0);
+            DateTime dateTimeTo = new DateTime(
+                currentDateTime.Year, currentDateTime.Month, currentDateTime.Day, 19, 0, 0);
+
+            if (currentDateTime > dateTimeFrom && currentDateTime < dateTimeTo)
+            {
+                _log.Log($"Выполнение метода EveryDayRun.");
+                EveryDayRun();
+                _log.Log($"Метод EveryDayRun выполнен успешно.");
+            }
+        }
+
+        private void EveryDayRun()
+        {
+            _ediDbContext.ExecuteProcedure("EDI.UPDATE_DISABLED_GOODS_MAPPINGS");
         }
     }
 }
